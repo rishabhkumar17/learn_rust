f6b5b8dd832554f12886b73a3f5e56f742594e1a
rishabhkumar17 <rishabhkaushal1796@gmail.com>
2023-05-31 22:42:19 +0530

    cargo - Rust's build system and package manager

---
+++ hello_rust/target/debug/deps/hello_rust-10e0c78cf78d8626.d
@@ -0,0 +1,5 @@
+/home/sage/learn_rust/hello_rust/target/debug/deps/hello_rust-10e0c78cf78d8626: src/main.rs
+
+/home/sage/learn_rust/hello_rust/target/debug/deps/hello_rust-10e0c78cf78d8626.d: src/main.rs
+
+src/main.rs:
---
+++ hello_rust/target/debug/hello_rust.d
@@ -0,0 +1,1 @@
+/home/sage/learn_rust/hello_rust/target/debug/hello_rust: /home/sage/learn_rust/hello_rust/src/main.rs
