f3f78046279811c7c0adb921d577d7212958603a
rishabhkumar17 <rishabhkaushal1796@gmail.com>
2023-05-31 23:00:19 +0530

    gitignore

--- hello_rust/target/debug/deps/hello_rust-37ecda1db85cdcc4.d
+++
@@ -1,5 +0,0 @@
-/home/sage/learn_rust/hello_rust/target/debug/deps/hello_rust-37ecda1db85cdcc4.rmeta: src/main.rs
-
-/home/sage/learn_rust/hello_rust/target/debug/deps/hello_rust-37ecda1db85cdcc4.d: src/main.rs
-
-src/main.rs:
