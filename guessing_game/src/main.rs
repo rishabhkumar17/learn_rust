19bb06bd127a122d911e117570ef79085ce63e94
rishabhkumar17 <rishabhkaushal1796@gmail.com>
2023-06-01 19:43:17 +0530

    Testing->Processing-a-guess

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,12 @@
-use std::io
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let mut guess = String::new(); 
+    // add mut to make a variable mutable
 
     io::stdin()
         .read_line(&mut guess)
