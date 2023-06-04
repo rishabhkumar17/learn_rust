1501e852f6c697b5a0bc2e9cd0edb911dedc904a
rishabhkumar17 <rishabhkaushal1796@gmail.com>
2023-06-05 00:16:14 +0530

    constants

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
-    let mut x = 5;
+    // let x = 5; // immutable
+    let mut x = 5; // mutable
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("{}",THREE_HOURS_IN_SECONDS);
 }
