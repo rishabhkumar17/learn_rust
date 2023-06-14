2309c1aa3db9aab915eff5bbbff67e6b52d00dd4
rishabhkumar17 <rishabhkaushal1796@gmail.com>
2023-06-14 08:17:44 +0530

    arrays

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     // let x = 5; // immutable
     let mut x = 5; // mutable
@@ -45,4 +47,32 @@ fn main() {
     println!("{s}");
 
     let a = [1, 2, 3, 4, 5];
+
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    let a = [3; 5]; // same as let a = [3, 3, 3, 3, 3];
+
+     let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
+
+
 }
