89aaaf238ec1140c42bf03c6c1a0e511eb3a0e0f
rishabhkumar17 <rishabhkaushal1796@gmail.com>
2023-06-20 00:13:51 +0530

    functions-with-return-values

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -10,6 +10,12 @@ fn main() {
     };
 
     println!("The value of y is: {y}");
+
+    let x = five(); // statement
+
+    let x = plus_one(5);
+
+    println!("The value of x is: {x}");
 }
 
 // fn another_function(x: i32) {
@@ -20,3 +26,10 @@ fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}", );
 }
 
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1 // expression
+}
