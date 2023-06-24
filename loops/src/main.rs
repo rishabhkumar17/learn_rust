0e725f2c769e31ed77b078cc7fc1b2f112fadfb8
rishabhkumar17 <rishabhkaushal1796@gmail.com>
2023-06-24 23:30:25 +0530

    returning-values-from-loops

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,7 +1,21 @@
 fn main() {
     // Repeating Code with loop
 
-    loop {
-        println!("again!");
-    }
+    // loop {
+    //     println!("again!");
+    // }
+
+    // Returning Values from Loops
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
 }
