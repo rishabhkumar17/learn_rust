75ab20dcb67a76c95a5ce1892e31c63b16218798
rishabhkumar17 <rishabhkaushal1796@gmail.com>
2023-06-26 21:30:11 +0530

    looping-through-a-collection->while

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -53,4 +53,15 @@ fn main() {
     }
 
     println!("LIFTOFF!!!");
+
+    // Looping Through a Collection with for
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
 }
